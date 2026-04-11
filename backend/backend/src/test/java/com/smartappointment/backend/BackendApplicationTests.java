4a75c537e885d21175011278e57bbe7f998485d9
Musfira Ahmed <ahmedmusfira3@gmail.com>
2026-04-11 19:06:44 +0500

    Fix project structure: moved all files to root

---
+++ backend/backend/src/test/java/com/smartappointment/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartappointment.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
